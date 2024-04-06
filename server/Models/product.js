b9b46ac1e988087c76f3cf76c95ffbe3f52a1f92
yash <yash.goodluck4@gmail.com>
2024-04-06 22:07:51 +0530

    first dev push

---
+++ server/Models/product.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  admin: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "admin",
+  },
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  imageURL: {
+    type: String,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+const Product = mongoose.model("product", productSchema);
+module.exports = Product;
