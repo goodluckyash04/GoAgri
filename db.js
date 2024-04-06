c991b1baeddb45b3142a04d52dcd71aa81765532
yash <yash.goodluck4@gmail.com>
2024-04-06 19:30:04 +0530

    vercel

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
-const mongoose = require('mongoose');
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, './.env') });
-const database = process.env.DATABASE
-const connectToMongo= ()=>{
-     mongoose.connect(database)
-    console.log("Connected to Mongo")
+const mongoose = require("mongoose");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
+const database = process.env.DATABASE;
+const connectToMongo = () => {
+  mongoose.connect(database);
+  console.log("Connected to Mongo");
+};
 
-}
-
-module.exports=connectToMongo;
+module.exports = connectToMongo;
