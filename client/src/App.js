f2adddc3cb537503e4140e203cda9c434ae41273
yash <yash.goodluck4@gmail.com>
2024-04-06 22:03:29 +0530

    first dev commit

--- client/src/App.js
+++ client/src/App.js
@@ -25,53 +25,52 @@ import Wishstate from "./Context/products/Wishstate";
 import Productdetail from "./Component/Productdetail";
 import Cartstate from "./Context/products/Cartstate";
 
-
-
-
 function App() {
-  const [alert, setAlert] = useState(null)
-  const showalert=(msg,type)=>{
-      setAlert({msg:msg,type:type})
-      setTimeout(()=>{
-       setAlert(null) 
-      },2000)
-  }
+  const [alert, setAlert] = useState(null);
+  const showalert = (msg, type) => {
+    setAlert({ msg: msg, type: type });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
   return (
-      <Productstate>
-        <Wishstate>
-          <Cartstate>
-            <BrowserRouter>
-              <div style={{ minHeight: "100px" }}>
-                <Header alert={showalert} />
-              </div>
-              <Alert alert={alert}/>
-              <Routes>
-                <Route path="/" element={<About />} />
-                <Route path="login" element={<Login alert={showalert} />} />
-                <Route path="signup" element={<SignUp alert={showalert}/>} />
-                <Route path="adminlogin" element={<AdminLogin />} />
-                <Route path="adminsignup" element={<AdminSignup />} />
-                <Route path="addproduct" element={<AddProduct />} />
-                <Route path="product" element={<Product  />} />
-                <Route path="cart" element={<Cart />} />
-                <Route path="wishlist" element={<Wishlist />} />
-                <Route path="myaccount" element={<Account alert={showalert}  />} />
-                <Route path="product/fruits" element={<Fruits />} />
-                <Route path="product/vegetables" element={<Vegetables />} />
-                <Route path="product/seeds" element={<Seeds />} />
-                <Route path="product/fertilizers" element={<Fertilizer />} />
-                <Route path="search" element={<Search />} />
-                <Route path="ordernow" element={<OrderNow alert={showalert}/>} />
-                <Route path="resetpassword" element={<Resetpsw alert={showalert} />} />
-                <Route path="product/:id" element={<Productdetail />} />
-                <Route path="searchpage" element={<SearchPage />} />
-                
-              </Routes>
-              <Footer />
-            </BrowserRouter>
-          </Cartstate>
-        </Wishstate>
-      </Productstate>
+    <Productstate>
+      <Wishstate>
+        <Cartstate>
+          <BrowserRouter>
+            <div style={{ minHeight: "100px" }}>
+              <Header alert={showalert} />
+            </div>
+            <Alert alert={alert} />
+            <Routes>
+              <Route path="/" element={<About />} />
+              <Route path="login" element={<Login alert={showalert} />} />
+              <Route path="signup" element={<SignUp alert={showalert} />} />
+              <Route path="adminlogin" element={<AdminLogin />} />
+              <Route path="adminsignup" element={<AdminSignup />} />
+              <Route path="addproduct" element={<AddProduct />} />
+              <Route path="product" element={<Product />} />
+              <Route path="cart" element={<Cart />} />
+              <Route path="wishlist" element={<Wishlist />} />
+              <Route path="myaccount" element={<Account alert={showalert} />} />
+              <Route path="product/fruits" element={<Fruits />} />
+              <Route path="product/vegetables" element={<Vegetables />} />
+              <Route path="product/seeds" element={<Seeds />} />
+              <Route path="product/fertilizers" element={<Fertilizer />} />
+              <Route path="search" element={<Search />} />
+              <Route path="ordernow" element={<OrderNow alert={showalert} />} />
+              <Route
+                path="resetpassword"
+                element={<Resetpsw alert={showalert} />}
+              />
+              <Route path="product/:id" element={<Productdetail />} />
+              <Route path="searchpage" element={<SearchPage />} />
+            </Routes>
+            <Footer />
+          </BrowserRouter>
+        </Cartstate>
+      </Wishstate>
+    </Productstate>
   );
 }
 
